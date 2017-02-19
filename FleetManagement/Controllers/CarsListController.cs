e49c3205c28094e516043d77d5199800e6fc660e
Uliana <shymchak1@gmail.com>
2017-02-19 13:36:57 +0200

    Added car & carList controllers

---
+++ FleetManagement/Controllers/CarsListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FleetManagement.Controllers
+{
+    public class CarsListController : Controller
+    {
+        //
+        // GET: /CarsList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
